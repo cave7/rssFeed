76c47cb474507aa606a999c39324fcf5019f2df1
silencedrop <jialinliu7@gmail.com>
2012-10-05 13:21:17 -0700

    first version

---
+++ rssToJson.php
@@ -0,0 +1,20 @@
+<?php
+function Parse ($url) {
+
+		$fileContents= file_get_contents($url);
+
+		$fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
+
+		$fileContents = trim(str_replace('"', "'", $fileContents));
+
+		$simpleXml = simplexml_load_string($fileContents);
+
+		$json = json_encode($simpleXml);
+
+		return $json;
+
+	}
+print Parse("http://vodo.net/feeds/public");
+
+
+ ?>
